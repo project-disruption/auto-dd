64c69c2730bb908c56738515b7cfa458ebce4da5
Tony Luisi <t.luisi@gmail.com>
2017-10-15 10:34:03 +1300

    added input form for income by month

--- src/App/Visualisations/IncomeByMonthTimePlot.js
+++ src/App/Visualisations/IncomeByMonthTimePlot.js
@@ -5,25 +5,46 @@ import TimeSeriesPlot from "./TimeSeriesPlot";
 
 export default class IncomeByMonthTimePlot extends React.Component {
   state = {
-    incomeByMonth: []
+    incomeByMonth: [],
+    fromDate: moment().format("YYYY-MM-DD"),
+    toDate: moment().format("YYYY-MM-DD"),
+    line: 'Income'
+
   };
   componentDidMount () {
-    request.get('/profitandlossbyline?fromDate=2016-09-01&toDate=2017-03-31&line=Income')
+    this.getData()
+  }
+  getData = () => {
+    const { fromDate, toDate, line } = this.state
+    request.get('/profitandlossbyline')
+      .query({ fromDate, toDate, line })
       .then((response) => {
         this.setState({ incomeByMonth: response.body })
       })
+
+  }
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
   render() {
     return (
-      <TimeSeriesPlot
-        data={this.state.incomeByMonth}
-        height={800}
-        margin={100}
-        selectX={datum => moment(datum.date).toDate()}
-        selectY={datum => datum.value}
-        width={800}
-        setTooltip={this.setTooltip}
-      />
+      <div>
+        <input name="fromDate" type="text" value={this.state.fromDate} onChange={this.handleChange}/>
+        <input name="toDate" type="text" value={this.state.toDate} onChange={this.handleChange}/>
+        <button onClick={this.getData}>Submit</button>
+        <TimeSeriesPlot
+          data={this.state.incomeByMonth}
+          height={800}
+          margin={100}
+          selectX={datum => moment(datum.date).toDate()}
+          selectY={datum => datum.value}
+          width={800}
+          setTooltip={this.setTooltip}
+        />
+      </div>
     );
   }
 }
