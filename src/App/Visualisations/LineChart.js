073d42662adafc9b5a5df6e22ea9896c765c424e
T Pearse <t.pearse@gmail.com>
2018-03-25 15:58:27 +1300

    Added legend and reference line to chart

--- src/App/Visualisations/LineChart.js
+++ src/App/Visualisations/LineChart.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import {LineChart, XAxis, YAxis, Tooltip, Line, Legend} from 'recharts'
+import {LineChart, XAxis, YAxis, Tooltip, Line, Legend, ReferenceLine} from 'recharts'
 
 export default class LineChartPlot extends React.Component {
   render () {
       const data = this.props.data
   return (
     <div>
-        <LineChart width={600} height={300} data={data}>
+        <LineChart width={700} height={300} data={data}>
             <XAxis dataKey="date" />
-            <YAxis  tickFormatter={(value) => `$${value}`}/>
+            <YAxis  tickFormatter={(value) => `$${value}`} />
             <Tooltip formatter={(value) => `$${value}`}/>
-            <Line stroke="#06ab12" type="monotone" dataKey={d => d["Total Income"]} />
-            <Line stroke="#d1c309" type="monotone" dataKey={d => d["Net Profit"]} />
+            <Legend />
+            <ReferenceLine y="0" stroke="gray" strokeDasharray="3 3"/>
+            <Line stroke="#06ab12" type="monotone" dataKey={"Total Income"} />
+            <Line stroke="#d1c309" type="monotone" dataKey={"Net Profit"} />
+            <Line stroke="#ed550b" type="monotone" dataKey={"Total Operating Expenses"} />
+
         </LineChart>
     </div>
   );
